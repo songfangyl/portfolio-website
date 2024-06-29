d9185e10d5dba6a0473606058e40268a1eb3d452
Song Fang Yi <songfangyi1210@gmail.com>
2024-06-29 13:17:08 +0800

    change the base file path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: "/2048-in-react",
+    basePath: "/Portfolio-Website",
     typescript: {
         ignoreBuildErrors: true,
     }
