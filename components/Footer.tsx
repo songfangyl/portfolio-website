7f4b58a378e5696c3d4443d55a5d50cbea2a1abb
Song Fang Yi <songfangyi1210@gmail.com>
2024-06-29 12:48:12 +0800

    Finalize footer and fix some minor bug

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,17 +2,12 @@ import React from "react";
 import { div } from "three/examples/jsm/nodes/Nodes.js";
 import BorderButton from "./ui/BorderButton";
 import { FaLocationArrow } from "react-icons/fa";
+import { socialMedia } from "@/data";
+import { profile } from "console";
 
 const Footer = () => {
     return (
         <footer className="w-full pt-20 pb-10" id="contact">
-            <div className="w-full absolute left-0 -bottom-72 min-h-96">
-                <img
-                    src="/footer-grid.svg"
-                    alt="grid"
-                    className="w-full h-full opacity-50"
-                />
-            </div>
             <div className="flex flex-col items-center">
                 <h1 className="heading lg:max-w-[45vw]">
                     Ready to take <span className="text-purple">your</span>{" "}
@@ -29,6 +24,26 @@ const Footer = () => {
                         position="right"
                     ></BorderButton>
                 </a>
+            </div>
+            <div className="flex mt-16 md:flex-row flex-col items-center justify-between">
+                <p className="md:text-base text-sm md:font-normal font-light">
+                    Copyright © 2024 Song Fang Yi
+                </p>
+                <div className="flex items-center md:gap-3 gap-6">
+                    {socialMedia.map((profile) => (
+                        <div
+                            key={profile.id}
+                            className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-150 bg-opacity-75 bg-black-200 rounded-lg border border-black-300 "
+                        >
+                            <img
+                                src={profile.img}
+                                alt={profile.img}
+                                width={20}
+                                height={20}
+                            />
+                        </div>
+                    ))}
+                </div>
             </div>
         </footer>
     );
