4b09605443ee545ba146516aea86582c87bc7e73
Song Fang Yi <songfangyi1210@gmail.com>
2024-06-29 14:38:34 +0800

    Upload file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/portfolio-website',
     reactStrictMode: true,
     typescript: {
         ignoreBuildErrors: true,
