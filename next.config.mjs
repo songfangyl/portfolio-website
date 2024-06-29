c9361d0a6fd70b75019fb097b2e0574c8b50964e
Song Fang Yi <songfangyi1210@gmail.com>
2024-06-29 14:33:09 +0800

    remove the base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: "/portfolio-website",
     reactStrictMode: true,
     typescript: {
         ignoreBuildErrors: true,
